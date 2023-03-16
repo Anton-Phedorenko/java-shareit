f5f98cf3b1eb832c93598f7c47e3b052a3b41567
Anton <anton.phedornko.01@yandex.ru>
2023-03-16 21:12:55 +0300

    первая попытка сдачи

--- src/main/java/ru/practicum/shareit/comment/service/CommentService.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.comment.service;
-
-import ru.practicum.shareit.comment.CommentDto;
-
-public interface CommentService {
-    CommentDto create(CommentDto commentDto, Long itemId, Long userId);
-}
--- src/main/java/ru/practicum/shareit/exception/WrongStateException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.exception;
-
-public class WrongStateException extends RuntimeException {
-    public WrongStateException(String error) {
-        super(error);
-    }
-}
